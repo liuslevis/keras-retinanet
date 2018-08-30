19307f25dfa371d7948092f92b31f4c43ea1b85e
richliu(刘笑江) <richliu@tencent.com>
2018-08-30 13:28:44 +0800

    test.py

--- keras_retinanet/bin/test.py
+++ keras_retinanet/bin/test.py
@@ -20,26 +20,28 @@ from keras_retinanet.models import load_model
 from keras_retinanet.utils.image import read_image_bgr, preprocess_image, resize_image
 from keras_retinanet.utils.visualization import draw_box, draw_caption
 from keras_retinanet.utils.colors import label_color
-
+from pathlib import Path
 import matplotlib.pyplot as plt
 import cv2
 import numpy as np
 import time
+import os
 
 def main():
-    backbone_name, model_path = 'mobilenet128', './snapshots/mobilenet128_1_csv_20.h5'
-    # backbone_name, model_path = 'mobilenet128', './snapshots/mobilenet128_0.5_csv_20.h5'
-    # backbone_name, model_path = 'resnet50', './snapshots/resnet50_csv_19.h5'
+    backbone_name, model_path = 'mobilenet128', './model/ad/ad.mobilenet128_1_csv_20.h5'
+    # backbone_name, model_path = 'mobilenet128', './model/mobilenet128_1_csv_10.h5'
+    # backbone_name, model_path = 'resnet50', './model/resnet50_csv_20.h5'
+
     model = load_model(model_path, backbone_name, convert=True)
     # print(model.summary())
-    labels_to_names = {0:'bilibili', 1:'douyin', 2:'qq', 3:'qr', 4:'toutiao', 5:'weibo', 6:'weixin', 7:'xigua', 8:'yy'}
-    
-    images = [
-        read_image_bgr('../FLVGetter/logo_train/images/2197526474-201808241033.sec64.jpg'),
-        read_image_bgr('../FLVGetter/logo_train/images/129841-201808241034.sec2.jpg'),
-    ] + [
-        read_image_bgr(f'../FLVGetter/logo_test/{i}.jpg') for i in range(1, 14)
-    ]
+    # labels_to_names = {0:'aipai', 1:'bilibili', 2:'douyin', 3:'douyu', 4:'huya', 5:'qq', 6:'qr', 7:'taobao', 8:'toutiao', 9:'wangyi', 10:'weibo', 11:'weixin', 12:'xigua', 13:'yy', }
+    labels_to_names = {0:'ad'}
+    paths = []
+    for root, dirs, files in os.walk(f'../FLVGetter/ad_test/images'):
+        for file in files:
+            if file.endswith('.jpg') or file.endswith('.jpg'):
+                paths.append(str(Path(root) / file))
+    paths = sorted(paths)
 
     # cap = cv2.VideoCapture('../FLVGetter/logo_test/1446750313-201808172105.flv')
     # images = []
@@ -48,8 +50,8 @@ def main():
     #     if not succ: break
     #     images.append(image)
 
-    for image in images:
-        # copy to draw on    
+    for path in paths:
+        image = read_image_bgr(path)
         draw = image.copy()
 
         # preprocess image for network
@@ -65,22 +67,28 @@ def main():
         boxes /= scale
 
         # visualize detections
+        captions = []
         for box, score, label in zip(boxes[0], scores[0], labels[0]):
             # scores are sorted so we can break
-            if score < 0.5:
+            if score < 0.2:
                 break
                 
             color = label_color(label)
             
             b = box.astype(int)
             draw_box(draw, b, color=color)
-            
             caption = "{} {:.3f}".format(labels_to_names[label], score)
+            captions.append(caption)
             draw_caption(draw, b, caption)
         
+        print(path, ','.join(captions))
 
-        cv2.imshow('(q)uit', cv2.resize(draw, (800, 450)))
+        winname = '(q)uit'
+        cv2.namedWindow(winname)
+        cv2.moveWindow(winname, 100, 100)
+        cv2.imshow(winname, resize_image(draw)[0])
         key = cv2.waitKey(0)
+
         if key == ord('q'):
             return
 
